ec385a1c9926b5de94a6ad47654b6ea920f2f4fe
Hujaifa21 <21hujaifa@gmail.com>
2022-08-18 18:56:05 +0600

    tabs created

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import { Routes, Route } from "react-router-dom";
-import './App.css';
-import Home from './pages/Home';
-import DiscussedDetails from './components/discussed/DiscussedDetails';
-import LatestDetails from './components/latest/LatestDetails';
-import SelectedDetails from './components/selected/SelectedDetails';
-import MostreadDetails from './components/mostread/MostreadDetails';
+import "./App.css";
+import Home from "./pages/Home";
+import DiscussedDetails from "./components/discussed/DiscussedDetails";
+import LatestDetails from "./components/latest/LatestDetails";
+import SelectedDetails from "./components/selected/SelectedDetails";
+import MostreadDetails from "./components/mostread/MostreadDetails";
 import Header from "./shared/Header";
 
 function App() {
   return (
-    <div>
-      <Header/>
+    <>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>} />
-
+        <Route path="/" element={<Home />} />
+        
         <Route path="news">
           <Route path="latest/:latestId" element={<LatestDetails />} />
           <Route path="mostread/:mostreadId" element={<MostreadDetails />} />
-          <Route path="discussed/:discussedId" element={<DiscussedDetails/>} />
+          <Route path="discussed/:discussedId" element={<DiscussedDetails />} />
           <Route path="selected/:selectedId" element={<SelectedDetails />} />
         </Route>
-
       </Routes>
-    </div>
+    </>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import SelectedNews from '../components/selected/SelectedNews';
 
 const Home = () => {
   const [news, setNews] = useState([]);
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
     fetch('/sample.json')
@@ -16,6 +17,37 @@ const Home = () => {
   return (
     <div>
 
+      <div className="tabs">
+        <div className="tab-list">
+          <span className="tab-head" onClick={() => setIndex(0)}>
+            সর্বশেষ
+          </span>
+          <span className="tab-head" onClick={() => setIndex(1)}>
+            পঠিত
+          </span>
+          <span className="tab-head" onClick={() => setIndex(2)}>
+            আলোচিত
+          </span>
+
+        </div>
+        <div className="tab-content" hidden={index != 0}>
+          <div className=''>
+            {news && news[0]?.latest?.items.map(latest => <LatestNews key={latest.id} latest={latest}></LatestNews>)}
+          </div>
+        </div>
+        <div className="tab-content" hidden={index != 1}>
+          <div className=''>
+            {news && news[0]?.mostread?.items.map(most => <MostreadNews key={most.id} most={most}></MostreadNews>)}
+          </div>
+        </div>
+        <div className="tab-content" hidden={index != 2}>
+          <div className=''>
+            {news && news[0]?.discussed?.items.map(discussion => <DiscussedNews key={discussion.id} discussion={discussion}></DiscussedNews>)}
+          </div>
+        </div>
+      </div>
+
+      {/* 
       <div className='' style={{ 'margin': '50px' }}>
         {news && news[0]?.discussed?.items.map(discussion => <DiscussedNews key={discussion.id} discussion={discussion}></DiscussedNews>)}
       </div>
@@ -26,9 +58,12 @@ const Home = () => {
 
       <div className='' style={{ 'margin': '50px' }}>
         {news && news[0]?.mostread?.items.map(most => <MostreadNews key={most.id} most={most}></MostreadNews>)}
-      </div>
+      </div> */}
 
       <div className='' style={{ 'margin': '50px' }}>
+
+<span className="selected">নির্বাচিত</span>
+
         {news && news[0]?.selected?.items.map(select => <SelectedNews key={select.id} select={select}></SelectedNews>)}
       </div>
 
